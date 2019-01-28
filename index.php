4be897fecbfdcc7e74598e6aa10fbf69496d00b9
Aritra Nirmal <aritra@coincafe.com>
2019-01-27 21:42:51 -0500

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aritracci
+ * Date: 1/27/19
+ * Time: 9:40 PM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mafia</title>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.3/js/foundation.js">
+</head>
+<body>
+
+</body>
+</html>
