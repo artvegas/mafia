6d7cc045e35490c96b6e2ea7706d1f7ce61294d2
Aritra Nirmal <aritranirmal@gmail.com>
2019-01-27 21:32:07 -0500

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.3/css/foundation.css">
+</head>
+<body>
+    <div class="rows">
+        <div class="small-2 columns">
+            hi
+        </div>
+        <div class="small-2 columns">
+            hi
+        </div>
+    </div>
+</body>
+</html>
